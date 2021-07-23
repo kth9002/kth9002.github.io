9a9d78a55c63bc61ea576a304aa29cd93bdcaa42
kth9002 <87624446+kth9002@users.noreply.github.com>
2021-07-23 15:00:45 +0900

    Add files via upload

---
+++ main/portfolio.js
@@ -0,0 +1,78 @@
+$(function(){
+///////////////////////////
+
+$("#main").fullpage({
+    anchors:['m01', 'm02', 'm03', 'm04', 'm05'],
+    afterLoad: function(origin, destination, direction){
+        //console.log(destination.index);
+        $(".section").eq(destination.index).addClass("on").siblings().removeClass("on");
+        $("nav li").eq(destination.index).addClass("on").siblings().removeClass("on");
+        if(destination.index===4) {
+            $("footer").css({color:"#fff"})
+        }else{
+            $("footer").removeAttr("style")
+        }
+    },
+});
+
+$("#Mopen").on("click", function(){
+    $(this).toggleClass("on");
+    $("#cover_nav").toggleClass("on")
+});
+
+$(".m01 .xi-pause").on("click", function(){
+    $(".m01_video").trigger("pause");
+});
+
+$(".m01 .xi-play").on("click", function(){
+    $(".m01_video").trigger("play");
+});
+
+$("#cover_nav .link a").on("click", function(){
+    $("#cover_nav").removeClass("on");
+    $("#Mopen").removeClass("on");
+});
+
+
+$(".pf01 .xi-pause").on("click", function(){
+    $(".p01_video").trigger("pause");
+});
+
+$(".pf01 .xi-play").on("click", function(){
+    $(".p01_video").trigger("play");
+});
+
+$(".pf02 .xi-pause").on("click", function(){
+    $(".p02_video").trigger("pause");
+});
+
+$(".pf02 .xi-play").on("click", function(){
+    $(".p02_video").trigger("play");
+});
+
+$(".pf_slider").slick({
+    arrows:false,
+    slidesToShow: 3,
+    dots:true,
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,    }
+          },
+        ]      
+});
+
+$(".pf03 i.xi-arrow-left").on("click", function(){
+    $(".pf_slider").slick("slickPrev");
+});
+$(".pf03 i.xi-arrow-right").on("click", function(){
+    $(".pf_slider").slick("slickNext");
+});
+
+
+
+
+
+////////////////////////////
+});
---
+++ web02/ja/main.js
@@ -0,0 +1,17 @@
+$(function(){
+
+
+    $("#Wrap").fullpage({
+        navigation: true,
+        afterLoad: function(origin, destination, direction){
+            $(".section").eq(destination.index).addClass("on").siblings().removeClass("on");
+        },
+    });
+
+
+
+
+
+
+
+});
